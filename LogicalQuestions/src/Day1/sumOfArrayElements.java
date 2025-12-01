30d0957e521ee39783848d44f3cf5a3c189e78b4
23samaira <khandelwalshreya64@gmail.com>
2025-12-01 14:13:38 +0530

    Sum of elements of Array

---
+++ LogicalQuestions/src/Day1/sumOfArrayElements.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class sumOfArrayElements {
+	
+	public static void main(String[] args) {
+		int arr[]= {10,11,12,20,13,30,14,40};
+		int sum =0;
+		for (int i=0; i< arr.length; i++) {
+			sum += arr[i];
+		}
+		System.out.println(sum);
+	}
+}
+
+
+	
